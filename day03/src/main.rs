6a96f4624d652983644c09d56be68764323b6dc6
Stephan Müller <stephan@notatoaster.org>
2022-12-10 19:57:01 +0100

    Day 03 part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -8,11 +8,18 @@ struct Rucksack {
 }
 
 impl Rucksack {
-    fn common_item(self) -> char {
+    fn common_item(&self) -> char {
         let first_chars : HashSet<char> = self.first_compartment.chars().collect();
         let second_chars : HashSet<char> = self.second_compartment.chars().collect();
 
         return *first_chars.intersection(&second_chars).next().unwrap();
+    }
+
+    fn all(&self) -> HashSet<char> {
+        let mut all : HashSet<char> = HashSet::new();
+        all.extend(self.first_compartment.chars());
+        all.extend(self.second_compartment.chars());
+        all
     }
 }
 
@@ -20,9 +27,10 @@ impl FromStr for Rucksack {
     type Err = ();
 
     fn from_str(text : &str) -> Result<Rucksack, ()> {
+        let split = text.len()/2;
         let parsed = Rucksack {
-            first_compartment: text.get(..text.len()/2).unwrap().to_string(),
-            second_compartment: text.get((text.len()/2)..).unwrap().to_string()
+            first_compartment: text.get(..split).unwrap().to_string(),
+            second_compartment: text.get(split..).unwrap().to_string()
         };
 
         Ok(parsed)
@@ -33,19 +41,30 @@ fn to_prio(c : char) -> i32 {
     match c {
         'a'..='z' => (c as i32)-96,
         'A'..='Z' => (c as i32)-38,
-        _        => 0
+        _         => 0
     }
 }
 
 fn main() {
     let txt = include_str!("..\\input.txt");
-    let sum : i32 = txt
+    let rucksacks : Vec<Rucksack> = txt
         .lines()
         .map(|ln| ln.parse::<Rucksack>().unwrap())
+        .collect();
+
+    let sum : i32 = rucksacks
+        .iter()
         .map(|r| r.common_item())
         .map(|c| to_prio(c))
         .sum();
     println!("Sum: {}", sum);
+
+    let sum2 : i32 = rucksacks
+        .chunks(3)
+        .map(|g| g.into_iter().map(|x| x.all()).reduce(|acc,x| acc.intersection(&x).map(|c| *c).collect()))
+        .map(|c| to_prio(c.unwrap().into_iter().next().unwrap()))
+        .sum();
+    println!("Sum2: {}", sum2);
 
     assert_eq!(sum, 8085);
 }
